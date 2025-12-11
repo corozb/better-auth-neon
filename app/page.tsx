ddacc534a6952dc97c89ace42b8bdac319132236
Cristian Orozco <cristianorozco.ben@gmail.com>
2025-12-11 08:07:18 -0500

    Initial Setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Page() {
+  return (
+    <div>
+      <Button variant="default" size="lg">
+        Click Me
+      </Button>
+    </div>
+  );
+}
